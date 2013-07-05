c5bfece8d592830c231869d4c60f725c41cb9f08
tiago <tiago.avila09@gmail.com>
2013-07-05 15:02:36 -0300

    comitando projeto de teste

---
+++ Mensagens/Mensagens/Models/ActionResultExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mensagens.Models
+{
+    public static class ActionResultExtensions
+    {
+        /// <summary>
+        /// Redireciona para uma ActionResult retornando uma mensagem de confirmação para a view
+        /// </summary>
+        /// <param name="actionResult"></param>
+        /// <param name="mensagem"></param>
+        /// <param name="classeAlert">classe do alerta a ser exibido (alert-error, alert-success, alert-info)</param>
+        /// <returns></returns>
+        public static ActionResult ComMensagem(this ActionResult actionResult, string mensagem, string classeAlert)
+        {
+            return new TempDataActionResult(actionResult, mensagem, classeAlert);
+        }
+    }
+}
---
+++ Mensagens/Mensagens/Models/TempDataActionResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mensagens.Models
+{
+    public class TempDataActionResult : ActionResult
+    {
+        private readonly ActionResult _actionResult;
+        private readonly string _mensagem;
+        private readonly string _classeAlert;
+
+        public TempDataActionResult(ActionResult actionResult, string mensagem, string classeAlert)
+        {
+            _actionResult = actionResult;
+            _mensagem = mensagem;
+            _classeAlert = classeAlert;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.Controller.TempData["Mensagem"] = _mensagem;
+            context.Controller.TempData["ClasseAlert"] = _classeAlert;
+            _actionResult.ExecuteResult(context);
+        }
+    }
+}
